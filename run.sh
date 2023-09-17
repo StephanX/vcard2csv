7b18d84b06a825c0334bdab28988253c9789883c
Stephan Budge <stephan@stephanx.net>
2023-09-17 14:37:38 -0700

    meh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ cd git/vcard2csv
 git pull
 cd /mnt/chromeos/MyFiles/Downloads
 FILENAME=$(ls -t *.vcf | head -n 1)
-${HOME}/bin/vcf2csv.py ${FILENAME} contacts.csv
+${HOME}/bin/vcf2csv.py "${FILENAME}" contacts.csv
